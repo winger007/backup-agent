9ada06c504f6eb9de589c613f26a60bc6d201bc3
root <root@code server>
2015-12-16 11:10:01 +0800

    Fix crontab environment issue
    In crontab, the python excute path is depends on where the python
    running the script,so we need to locate the file path instead of
    python path
    
    Signed-off-by: root <root@code server>

--- backup-agent.py
+++ backup-agent.py
@@ -35,8 +35,7 @@ class Config(object):
         if specified is True:
             config_file = self.config_file
         else:
-            config_file = os.environ.get('SWIFTCLIENT_CONFIG_FILE',
-                                     './conf/backup.conf')
+            config_file = os.path.dirname(os.path.realpath(__file__)) + "/conf/backup.conf"
         config = configparser.SafeConfigParser({'auth_version': '1'})
         config.read(config_file)
         if config.has_section('swiftconf'):
